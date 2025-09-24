fae492c8a5f9de3a43fdfa168697eae3e18b4aff
Fabio Bravo <157680743+Fbiosb@users.noreply.github.com>
2025-09-23 20:35:34 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// nueva version con tiempo y contando
+// descagada la base, vinculado el git, trabajar el java
+// luego el array amigos
+// implementar funciones para agregar amigos, actualizar la lista, sortear amigos.
+// crear un readme "interesante?????"
+
+let amigos = []
