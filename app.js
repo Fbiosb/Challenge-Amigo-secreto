516636ef5dc88a63ad009222bd606918cc5bdbe0
Fabio Bravo <157680743+Fbiosb@users.noreply.github.com>
2025-09-23 21:02:35 -0500

    correcciones varias

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
 // implementar funciones para agregar amigos, actualizar la lista, sortear amigos.
 // crear un readme "interesante?????"
 
-const { act } = require("react");
-
 let amigos = [];
 
 //problema del git solucionado. error en el master y main
@@ -15,13 +13,13 @@ let amigos = [];
 function agregarAmigo() {
     const input = document.getElementById('amigo');
     const nombre = input.value.trim();
-    if (nombre === '') {
-       amigos. push(nombre);
+    if (nombre) {
+       amigos.push(nombre);
        actualizarLista();
        input.value = '';
     }   
 }       
-
+        //correccion de la funcion, creo que tenia elementos de mas
 // no estaba bien escrito. al parecer se me pasó
 // ahora funcion de actualizar lista. esto lo puse encima del anterior
 
@@ -35,7 +33,7 @@ function actualizarLista() {
     });
 }
     // ahora si creo que esta solucionado.... vamos a probar
-
+        //esta parte esta bien. seguimos revisando abajo
 
 
 //sortear nombres.. 
@@ -45,19 +43,22 @@ function sortearAmigo() {
         alert('No hay amigos en la lista para sortear. Por favor inserte un nombre válido');
         return;
     }
-    const indice = Math.floor(Math.random() * amigos.length);
-    const amigoSorteado = amigos[indice];
-    alert(`El amigo sorteado es: ${amigoSorteado}`);
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
 }
 
-// funcion para el boton de enter
 
-document.getElementById('nombre-amigo').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        e.preventDefault(); // Evita que se envíe el formulario si está dentro de uno
-        document.getElementById('agregar-btn').click(); // Simula el clic en el botón
+// funcion para el boton de enter
+document.getElementById('amigo').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        agregarAmigo();
+        
     }      
 });
 
 // no esta jalando nombres :(
     // vamos a correr de nuevo v. 2.0
+        // corremos nuevamente
+        
