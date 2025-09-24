427519a0a0026193d99dc934a9f963bedf832f89
Fabio Bravo <157680743+Fbiosb@users.noreply.github.com>
2025-09-23 20:40:40 -0500

    crea funcion de agregar amigos

--- app.js
+++ app.js
@@ -4,4 +4,20 @@
 // implementar funciones para agregar amigos, actualizar la lista, sortear amigos.
 // crear un readme "interesante?????"
 
-let amigos = []
+let amigos = [];
+
+//problema del git solucionado. error en el master y main
+// continuamos
+
+
+function agregarAmigo() {
+    const lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+    amigos.forEach((amigo) => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+//sortear nombres.. 
